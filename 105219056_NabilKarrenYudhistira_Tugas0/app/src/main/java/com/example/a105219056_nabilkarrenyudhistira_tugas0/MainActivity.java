1d5674a63c6aa83dc839e512cf16661747427800
Nabilkarren <nabilgunner@gmail.com>
2022-03-10 20:32:59 +0700

    Modul 0

---
+++ 105219056_NabilKarrenYudhistira_Tugas0/app/src/main/java/com/example/a105219056_nabilkarrenyudhistira_tugas0/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a105219056_nabilkarrenyudhistira_tugas0;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
